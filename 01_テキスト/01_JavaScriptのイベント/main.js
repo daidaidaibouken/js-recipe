8079ed36e958fa3f22b59aee63f843be3f8c1644
dai <21ca082e@rikkyo.ac.jp>
2023-07-22 22:01:54 +0900

    [カウンター]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,24 @@
+// イベントが発生する要素を取得する
+const button = document.getElementById("button")
+
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+
+// 関数を登録
+button.onclick = alertMessage
+
+// input 要素と input イベント
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (e) {
+  console.log(e.target.value)
+}
+
+document.onkeydown = function (e) {
+  console.log(e.key)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
